1cc1c2b6841ab4bfc486de4d6b4f6462f0b123da
mac <guozy2008@126.com>
2018-12-07 21:56:08 +0800

    hotfix

--- src/main/java/com/study/first/FirstApplication.java
+++ src/main/java/com/study/first/FirstApplication.java
@@ -16,5 +16,7 @@ public class FirstApplication extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         // 注意这里要指向原先用main方法执行的Application启动类
         return builder.sources(FirstApplication.class);
+
+        //hotfix 分支ß
     }
 }
